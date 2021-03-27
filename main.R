35661fd3b1e2d386257524b39eb8331315618543
michaltkaczyk <michaltkaczyk@outlook.com>
2021-03-27 20:53:12 +0100

    Update main.R
    This seems to work. Does not inform on the winner yet.

--- main.R
+++ main.R
@@ -1,32 +1,35 @@
-cards_1 <- sample(rep(1:13, 2), 26, FALSE)
-cards_2 <- sample(rep(1:13, 2), 26, FALSE)
+cards <- list(
+    cards_1 = sample(rep(1:13, 2), 26, FALSE),
+    cards_2 = sample(rep(1:13, 2), 26, FALSE),
+    stash_1 = c(),
+    stash_2 = c())
 
-stash_1 <- c()
-stash_2 <- c()
-
-play_cards <- function(cards_1, cards_2) {
-    if (cards_1[1] > cards_2[1]) {
+play_cards <- function(cards) {
+    if (cards$cards_1[1] > cards$cards_2[1]) {
         print("Player 1 wins")
-        cards_1 <<- c(cards_1[-1], cards_1[1], cards_2[1])
-        cards_2 <<- cards_2[-1]
+        cards$cards_1 <- c(cards$cards_1[-1], cards$stash_1, cards$stash_2, cards$cards_1[1], cards$cards_2[1])
+        cards$cards_2 <- cards$cards_2[-1]
+        cards$stash_1 <- c()
+        cards$stash_2 <- c()
     } else {
-        if (cards_1[1] < cards_2[1]) {
+        if (cards$cards_1[1] < cards$cards_2[1]) {
             print("Player 2 wins")
-            cards_2 <<- c(cards_2[-1], cards_2[1], cards_1[1])
-            cards_1 <<- cards_1[-1]
+            cards$cards_2 <- c(cards$cards_2[-1], cards$stash_2, cards$stash_1, cards$cards_2[1], cards$cards_1[1])
+            cards$cards_1 <- cards$cards_1[-1]
+            cards$stash_1 <- c()
+            cards$stash_2 <- c()
         } else {
             print("Battle")
-            stash_1 <<- c(stash_1, cards_1[1:2])
-            stash_2 <<- c(stash_2, cards_2[1:2])
-            cards_1 <<- cards_1[-(1:2)]
-            cards_2 <<- cards_2[-(1:2)]
-            play_cards(cards_1, cards_2)
+            cards$stash_1 <- c(cards$stash_1, cards$cards_1[1:2])
+            cards$stash_2 <- c(cards$stash_2, cards$cards_2[1:2])
+            cards$cards_1 <- cards$cards_1[-(1:2)]
+            cards$cards_2 <- cards$cards_2[-(1:2)]
+            play_cards(cards)
         }
     }
-    stash_1 <- c()
-    stash_2 <- c()
+    return(cards)
 }
 
-for (rounds in 1:100) {
-    play_cards(cards_1, cards_2)
+for (rounds in 1:1000) {
+    cards <- play_cards(cards)
 }
