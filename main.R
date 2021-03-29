228368f0dcc36e42a36b308e4df987f8f72390d8
michaltkaczyk <michaltkaczyk@outlook.com>
2021-03-29 13:06:07 +0200

    Update main.R
    Add deck and report_victory() function

--- main.R
+++ main.R
@@ -1,18 +1,26 @@
+deck <- sample(rep(1:13, 4))
+
 cards <- list(
-    cards_1 = sample(rep(1:13, 2), 26, FALSE),
-    cards_2 = sample(rep(1:13, 2), 26, FALSE),
+    cards_1 = deck[1:26],
+    cards_2 = deck[27:52],
     stash_1 = c(),
     stash_2 = c(),
-    winner = 0)
+    winner = 0,
+    counter = 0)
+
+report_victory <- function(cards) {
+    cat("Player", cards$winner, "wins the game after", cards$counter, "rounds!\n") 
+}
 
 play_cards <- function(cards) {
     if (cards$winner == 0) {
         if (cards$cards_1[1] > cards$cards_2[1]) {
             if (length(cards$cards_2) == 1) {
                 cards$winner <- 1
-                cat("Player 1 wins the game!\n")
+                report_victory(cards)
             } else {
                 cat("Player 1 wins:", cards$cards_1[1], "beats", cards$cards_2[1], "\n")
+                cards$counter <- cards$counter + 1
                 cards$cards_1 <- c(cards$cards_1[-1], cards$stash_1, cards$stash_2, cards$cards_1[1], cards$cards_2[1])
                 cards$cards_2 <- cards$cards_2[-1]
                 cards$stash_1 <- c()
@@ -21,10 +29,11 @@ play_cards <- function(cards) {
         } else {
             if (cards$cards_1[1] < cards$cards_2[1]) {
                 if (length(cards$cards_1) == 1) {
-                    cat("Player 2 wins the game!\n")
                     cards$winner <- 2
+                    report_victory(cards)
                 } else {
                     cat("Player 2 wins:", cards$cards_2[1], "beats", cards$cards_1[1], "\n")
+                    cards$counter <- cards$counter + 1
                     cards$cards_2 <- c(cards$cards_2[-1], cards$stash_2, cards$stash_1, cards$cards_2[1], cards$cards_1[1])
                     cards$cards_1 <- cards$cards_1[-1]
                     cards$stash_1 <- c()
@@ -33,12 +42,12 @@ play_cards <- function(cards) {
             } else {
                 cat("Battle!\n")
                 if (length(cards$cards_2) == 1) {
-                    cat("Player 1 wins the game!\n")
                     cards$winner <- 1
+                    report_victory(cards)
                 } else {
                     if (length(cards$cards_1) == 1) {
-                        cat("Player 2 wins the game!\n")
                         cards$winner <- 2
+                        report_victory(cards)
                     } else {
                         cards$stash_1 <- c(cards$stash_1, cards$cards_1[1:2])
                         cards$stash_2 <- c(cards$stash_2, cards$cards_2[1:2])
